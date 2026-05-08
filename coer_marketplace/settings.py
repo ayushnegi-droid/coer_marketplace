295702a90e0e25e22673c2d0e9960733abfcac0b
Ayush Negi <ayushnegi@Ayushs-MacBook-Air-2.local>
2026-05-08 10:21:14 +0530

    add postgresql database

--- coer_marketplace/settings.py
+++ coer_marketplace/settings.py
@@ -60,11 +60,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'coer_marketplace.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
